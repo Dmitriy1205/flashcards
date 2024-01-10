44411c98bb1a456c5a857363cf338e173d1cc19b
Dmitriy Plashkin <66801350+Dmitriy1205@users.noreply.github.com>
2024-01-10 14:28:08 +0200

    apple_sign_in_web

--- lib/domain/repositories/auth/auth_repository_impl.dart
+++ lib/domain/repositories/auth/auth_repository_impl.dart
@@ -34,13 +34,19 @@ class AuthRepositoryImpl implements AuthRepository {
   Future<void> signInWithApple() async {
     final rawNonce = generateNonce();
     final nonce = _sha256ofString(rawNonce);
+    var redirectURL =
+        "https://flashcards-5984c.firebaseapp.com/__/auth/handler";
+    var clientID = "com.pandascode.flashcard";
 
     try {
-      final appleCredential =
-          await SignInWithApple.getAppleIDCredential(scopes: [
-        AppleIDAuthorizationScopes.email,
-        AppleIDAuthorizationScopes.fullName,
-      ], nonce: nonce);
+      final appleCredential = await SignInWithApple.getAppleIDCredential(
+          scopes: [
+            AppleIDAuthorizationScopes.email,
+            AppleIDAuthorizationScopes.fullName,
+          ],
+          nonce: nonce,
+          webAuthenticationOptions: WebAuthenticationOptions(
+              clientId: clientID, redirectUri: Uri.parse(redirectURL)));
       final oauthCredential = OAuthProvider("apple.com").credential(
         idToken: appleCredential.identityToken,
         rawNonce: rawNonce,
