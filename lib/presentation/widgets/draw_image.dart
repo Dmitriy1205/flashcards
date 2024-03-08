ff96d3ba3e532ac3fb9f7d5ee9e1150a4608205c
yarshaqa <yarshaqa@gmail.com>
2024-03-08 14:34:47 +0200

    fix painting area

--- lib/presentation/widgets/draw_image.dart
+++ lib/presentation/widgets/draw_image.dart
@@ -13,10 +13,10 @@ mixin DrawImage<T extends StatefulWidget> on State<T> {
 
   void showOverlay(BuildContext context) {
     _dialogOverlayEntry = _createDialogOverlayEntry(context);
-    _colorPickerOverlayEntry = _createColorPickerOverlayEntry(context);
+    //_colorPickerOverlayEntry = _createColorPickerOverlayEntry(context);
 
     Overlay.of(context).insert(_dialogOverlayEntry!);
-    Overlay.of(context).insert(_colorPickerOverlayEntry!);
+    //Overlay.of(context).insert(_colorPickerOverlayEntry!);
   }
 
   void removeOverlay() {
@@ -25,78 +25,84 @@ mixin DrawImage<T extends StatefulWidget> on State<T> {
   }
 
   OverlayEntry _createDialogOverlayEntry(BuildContext context) {
-    return OverlayEntry(builder: (context) {
-      return Positioned(
-        top: MediaQuery.of(context).size.height / 5.5,
-        left: MediaQuery.of(context).size.width / 10,
-        child: Material(
-          color: Colors.transparent,
-          child: Container(
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15),
-                border: Border.all(color: Colors.black)),
-            width: MediaQuery.of(context).size.width * 0.8,
-            height: MediaQuery.of(context).size.height * 0.6,
-            // padding: EdgeInsets.all(18),
-            child: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    TextButton(
-                      onPressed: () => removeOverlay(),
-                      child: Text('Cancel'),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        // TODO: Implement your confirm logic here
-                        removeOverlay();
-                      },
-                      child: Text('Confirm'),
-                    ),
-                  ],
-                ),
-                PaintingArea(color: _selectedColor ),
-              ],
+    return OverlayEntry(
+        builder: (context) {
+      return StatefulBuilder(
+        builder: (context,setState) {
+          return Stack(
+          children: [
+          Positioned(
+          bottom: 0,
+          child: Material(
+            color: Colors.transparent,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 85,
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                  border: Border.symmetric(
+                      horizontal: BorderSide(color: Colors.black))),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: colors.map((color) {
+                  return GestureDetector(
+                    onTap: () {
+                      // Handle the color selection
+                      print('Color $color selected');
+                    },
+                    child: buildColorChoice(color, setState),
+                  );
+                }).toList(),
+              ),
             ),
           ),
         ),
+            Positioned(
+              top: MediaQuery.of(context).size.height / 5.5,
+              left: MediaQuery.of(context).size.width / 10,
+              child: Material(
+                color: Colors.transparent,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(color: Colors.black)),
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  // padding: EdgeInsets.all(18),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          TextButton(
+                            onPressed: () => removeOverlay(),
+                            child: Text('Cancel'),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              // TODO: Implement your confirm logic here
+                              removeOverlay();
+                            },
+                            child: Text('Confirm'),
+                          ),
+                        ],
+                      ),
+                      PaintingArea(color: _selectedColor ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ]
+        );
+        },
       );
     });
   }
 
-  OverlayEntry _createColorPickerOverlayEntry(BuildContext context) {
-    return OverlayEntry(builder: (context) {
-      return Positioned(
-        bottom: 0,
-        child: Material(
-          color: Colors.transparent,
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: 85,
-            decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(15),
-                border: Border.symmetric(
-                    horizontal: BorderSide(color: Colors.black))),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: colors.map((color) {
-                return GestureDetector(
-                  onTap: () {
-                    // Handle the color selection
-                    print('Color $color selected');
-                  },
-                  child: buildColorChoice(color),
-                );
-              }).toList(),
-            ),
-          ),
-        ),
-      );
-    });
-  }
+
 
   List<Color> colors = [
     Colors.black,
@@ -107,7 +113,7 @@ mixin DrawImage<T extends StatefulWidget> on State<T> {
     AppColors.redPicker
   ];
 
-  Widget buildColorChoice(Color color) {
+  Widget buildColorChoice(Color color, Function setState) {
     return GestureDetector(
       onTap: () {
         print('object $color');
@@ -115,9 +121,9 @@ mixin DrawImage<T extends StatefulWidget> on State<T> {
           setState(() {
             _selectedColor = color;
           });
-
         };
         _selectedColor = color;
+        setState((){});
       },
       child: Container(
         width: 25,
